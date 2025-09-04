e4f5f2ecd6de9e4ffb5d2bcb91c6d102051dc1b0
mariana <mariana@192.168.0.234>
2025-09-04 19:39:39 -0300

    reestruturando pacotes

---
+++ OneDrive/Documentos/NetBeansProjects/ProjetoTeste/src/main/java/hj/projetoteste/Pessoa.java
@@ -0,0 +1,8 @@
+
+package hj.projetoteste;
+
+public class Pessoa {
+    String nome;
+    int idade;
+    String sexo;
+}
---
+++ OneDrive/Documentos/NetBeansProjects/ProjetoTeste/src/main/java/hj/projetoteste/ProjetoTeste.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package hj.projetoteste;
+
+/**
+ *
+ * @author mariana
+ */
+public class ProjetoTeste {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
